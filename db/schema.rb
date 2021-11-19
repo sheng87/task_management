f529d180d90ceeaaaf738f5b19463ad80fc55361
sheng87 <4004171022kyu@gmail.com>
2021-11-19 15:36:36 +0800

    task model validation test

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_015003) do
+ActiveRecord::Schema.define(version: 2021_11_19_033319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "start"
     t.datetime "end"
-    t.text "content"
+    t.string "content", limit: 100
     t.string "label"
     t.string "priority"
     t.string "status"

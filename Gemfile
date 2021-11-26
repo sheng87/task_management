954e8e62d491b820519d2f03ca122fcd8f937bbf
sheng87 <4004171022kyu@gmail.com>
2021-11-26 19:41:11 +0800

    user's email && password validation

--- Gemfile
+++ Gemfile
@@ -21,10 +21,11 @@ gem 'aasm', '~> 5.2'
 gem 'ransack', '~> 2.4'
 gem 'kaminari', '~> 1.2', '>= 1.2.1'
 gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
+gem 'bcrypt', '~> 3.1.7'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  root to: 'tasks#home'
   resources :tasks
-  resources :users  
+  resources :users 
+  get 'signup' => 'users#new'
+  get  'login'   => 'sessions#new'
+  post 'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy' 
 end
